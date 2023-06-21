697ea5dbbe4ed39eebfa795d138c38db7c28a285
fadi <fadisam506@gmail.com>
2023-06-21 15:51:57 +0300

    Add project files.

---
+++ Timetable.Web/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Timetable.Web.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
