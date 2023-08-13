8120a65ab19f8567897176b8886ccd1829bfdb48
ayham-ghad <ayhamaboghad3@gmail.com>
2023-08-13 21:02:44 +0300

    modulation

---
+++ Timetable.Web/Pages/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Timetable.RazorWeb.Pages.Account
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
